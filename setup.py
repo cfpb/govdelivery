4adda163da5a40fbdb0bae356e091e94e61448e4
Ross M Karchner <ross@karchner.com>
2013-12-12 13:41:41 -0500

    Update setup.py
    This isn't the Django app

--- setup.py
+++ setup.py
@@ -13,23 +13,20 @@ def read_file(filename):
 
 
 setup(
-    name='django-govdelivery',
-    version=__import__('django-govdelivery').__version__,
+    name='govdelivery',
+    version=__import__('govdelivery').__version__,
     author='CFPB',
     author_email='tech@cfpb.gov',
     packages=['django-govdelivery'],
     include_package_data=True,
     description=u'Django app for interacting with the govdelivery api',
     classifiers=[
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Intended Audience :: Developers',
         'Programming Language :: Python',      
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Framework :: Django',
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
     ],
-    long_description=read_file('README.rst'),
     zip_safe=False,
 )
