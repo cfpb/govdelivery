de6c3f6885ece456382266b35ea31db6b2deeb24
Ross Karchner <ross.karchner@cfpb.gov>
2013-12-13 09:02:56 -0500

    housekeeping

--- setup.py
+++ setup.py
@@ -2,24 +2,14 @@ import os
 from setuptools import setup, find_packages
 
 
-def read_file(filename):
-    """Read a file into a string"""
-    path = os.path.abspath(os.path.dirname(__file__))
-    filepath = os.path.join(path, filename)
-    try:
-        return open(filepath).read()
-    except IOError:
-        return ''
-
-
 setup(
     name='govdelivery',
     version=__import__('govdelivery').__version__,
     author='CFPB',
     author_email='tech@cfpb.gov',
-    packages=['django-govdelivery'],
+    packages=['govdelivery'],
     include_package_data=True,
-    description=u'Django app for interacting with the govdelivery api',
+    description=u'python module for interacting with the govdelivery api',
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',      
