3d501611c5ba3ce75e933730cfbc028de70f234c
Scott Cranfill <scott.cranfill@cfpb.gov>
2019-11-20 17:14:55 -0500

    Update README and setup.py to reflect current state
    - Python 3 now supported
    - Tests now run with tox

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Communications :: Email',
     ],
     zip_safe=False,
